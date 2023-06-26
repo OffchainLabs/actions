bd0fd7ceb640175a7b8087bccfafbac9f23f8ea7
gzeon <im@gzeon.dev>
2023-06-26 22:22:53 +0800

    fix: wait for token bridge deployment

---
+++ run-nitro-test-node/waitForTokenBridge.sh
@@ -0,0 +1,10 @@
+# poll the token bridge deployment file until we get a 0 return code
+while true
+do
+  docker-compose -f ./nitro-testnode/docker-compose.yaml run --entrypoint sh tokenbridge -c "cat localNetwork.json"
+  if [ "$?" -eq "0" ]; then
+    exit 0
+  else
+    sleep 10
+  fi
+done
