5f8d53bbd95a1a54bbeab29bee24da1ac58f2db1
Chris Buckland <cpbuckland88@gmail.com>
2023-10-19 18:19:12 +0200

    Added 8545 logging

--- run-nitro-test-node/waitForNitro.sh
+++ run-nitro-test-node/waitForNitro.sh
@@ -2,6 +2,8 @@
 while true
 do
   curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' 'http://localhost:8545'
+  echo $?
+  curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' 'http://localhost:8547'
   if [ "$?" -eq "0" ]; then
     exit 0
   else
