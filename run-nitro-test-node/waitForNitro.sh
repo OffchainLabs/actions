f8d83a3f1081224b6df15f711d10d8dbe3bf7cf9
Chris Buckland <cpbuckland88@gmail.com>
2023-10-19 18:21:31 +0200

    Go back to 8545 only

--- run-nitro-test-node/waitForNitro.sh
+++ run-nitro-test-node/waitForNitro.sh
@@ -2,8 +2,6 @@
 while true
 do
   curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' 'http://localhost:8545'
-  echo $?
-  curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' 'http://localhost:8547'
   if [ "$?" -eq "0" ]; then
     exit 0
   else
