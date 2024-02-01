18e732a3387c4426c2a281b9dab03e48ab435d0d
Chris Buckland <cpbuckland88@gmail.com>
2024-02-01 15:14:38 +0000

    Try master

--- run-nitro-test-node/waitForTokenBridge.sh
+++ run-nitro-test-node/waitForTokenBridge.sh
@@ -1,8 +1,8 @@
 # poll the token bridge deployment file until we get a 0 return code
 while true
 do
-  docker compose -f ./nitro-testnode/docker-compose.yaml run --entrypoint sh tokenbridge -c "echo 'tokenbridge up';"
-  if [ "$?" -eq "tokenbridge up" ]; then
+  docker-compose -f ./nitro-testnode/docker-compose.yaml run --entrypoint sh tokenbridge -c "if [ -f localNetwork.json ]; then cat localNetwork.json; else cat network.json; fi"
+  if [ "$?" -eq "0" ]; then
     exit 0
   else
     sleep 10
