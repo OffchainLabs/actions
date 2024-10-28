984e1effee39f9bcb69e8e099c765b14906da6a4
gzeon <im@gzeon.dev>
2024-10-29 04:51:34 +0900

    feat: use detach

--- run-nitro-test-node/waitForNitro.sh
+++ run-nitro-test-node/waitForNitro.sh
@@ -2,12 +2,18 @@
 
 PORT=${1:-8547}
 
-while true
+# timeout after around 10 minutes
+for i in {1..60}
 do
-  curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' "http://localhost:$PORT"
+  curl --silent -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' "http://localhost:$PORT"
   if [ "$?" -eq "0" ]; then
+    echo "nitro ready at port $PORT"
     exit 0
   else
+    echo "nitro not ready yet at port $PORT"
     sleep 10
   fi
 done
+echo "exiting: printing docker logs"
+docker compose -f ./nitro-testnode/docker-compose.yaml logs
+exit 1
--- run-nitro-test-node/waitForTokenBridge.sh
+++ run-nitro-test-node/waitForTokenBridge.sh
@@ -1,10 +1,15 @@
 # poll the token bridge deployment file until we get a 0 return code
-while true
+# timeout after around 10 minutes
+for i in {1..60}
 do
   docker compose -f ./nitro-testnode/docker-compose.yaml run --entrypoint sh tokenbridge -c "cat $1"
   if [ "$?" -eq "0" ]; then
     exit 0
   else
+    echo "token bridge not ready yet $1"
     sleep 10
   fi
 done
+echo "exiting: printing docker logs"
+docker compose -f ./nitro-testnode/docker-compose.yaml logs tokenbridge
+exit 1
